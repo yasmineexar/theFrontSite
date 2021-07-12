6ef015da1a04645f44acdca817a900bcda5a2898
Maryam CHENAOUI <maryam.chenaoui.dz@viacesi.fr>
2021-07-12 12:34:23 +0100

    edit entreprises

--- controllers/entreprises.js
+++ controllers/entreprises.js
@@ -13,8 +13,8 @@ exports.read = (req, res) => {
                 offres = results.filter((e) => {
                     return e.Id_entrepold == entreprises[0].Id_entrepold
                 })
-                return res.render('entreprises/profil', { entreprises: entreprises[0], offres })
             })
+            return res.render('entreprises/profil', { entreprises: entreprises[0], offres })
         })
     }
     return res.render('entreprises/all')
@@ -24,6 +24,12 @@ exports.delet = (req, res) => {
 }
 exports.update = (req, res) => {
 
+}
+exports.edit = (req, res) => {
+    return Entreprise.getById(req.params.id).then((entreprises) => {
+        console.log(entreprises)
+        return res.render('entreprises/edit', { entreprises: entreprises[0] })
+    })
 }
 exports.add = (req, res) => {
     return res.render('entreprises/add')
@@ -44,4 +50,21 @@ exports.creat = (req, res) => {
         return res.redirect('add')
     })
 }
-
+exports.update = (req, res) => {
+    const Raison_social = req.body.Raison_social
+    const Secteur_activite = req.body.Secteur_activite
+    const Site_web = req.body.Site_web
+    const Localite = req.body.Localite
+    const Description = req.body.Description
+    const Id_entrepold = req.params.id
+    let entreprise = new Entreprise()
+    entreprise.Raison_social = Raison_social
+    entreprise.Secteur_activite = Secteur_activite
+    entreprise.Site_web = Site_web
+    entreprise.Localite = Localite
+    entreprise.Description = Description
+    entreprise.Id_entrepold = Id_entrepold
+    entreprise.update().then(() => {
+        return res.redirect('edit')
+    })
+}
--- routes/entreprise.js
+++ routes/entreprise.js
@@ -1,11 +1,15 @@
 const router = require('express').Router()
-const { creat, add, read, update, delet } = require('../controllers/entreprises')
+const { creat, add, edit, read, update, delet } = require('../controllers/entreprises')
+
+router.get('/add', add)
+router.post('/add', creat)
 
 router.get('/', read)
-router.get('/add', add)
 router.get('/:id', read)
-router.post('/add', creat)
+
 router.post('/:id', update)
+router.get('/edit/:id', edit)
+
 router.delete('/:id', delet)
 
 module.exports = router
