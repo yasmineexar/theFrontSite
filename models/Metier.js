6b93f9d656a96ebfd847fb9ce539800d287f93b8
Maryam CHENAOUI <maryam.chenaoui.dz@viacesi.fr>
2021-07-28 00:37:34 +0100

    jb

--- models/Metier.js
+++ models/Metier.js
@@ -35,7 +35,7 @@ module.exports = class Metier extends Model {
   }
   update = () => {
     return new Promise((resolve, reject) => {
-      this.dbconnection.query(`UPDATE ${this.tablename} SET Description = ?, Image = ?, Nom = ?, Id_faculte = ? where Id_metier = ${this.Id_metier}`, [this.Description, this.Image, this.Nom, this.Id_faculte], (error, results, fields) => {
+      this.dbconnection.query(`UPDATE ${this.tablename} SET Description = ?, Nom = ?, Id_faculte = ? where Id_metier = ${this.Id_metier}`, [this.Description, this.Nom, this.Id_faculte], (error, results, fields) => {
         if (error) return reject(error)
         resolve(results)
       })
